1c0b75fa2a8b142f44cd1ca6bbcf1a3cf4ddea08
Vinh Do <vinhdo07@gmail.com>
2019-02-28 23:16:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,8 +3,12 @@ require 'nokogiri'
 require 'pry'
 
 doc = Nokogiri::HTML('../fixtures/kickstarter')
-binding.pry
 
 def create_project_hash
   projects = {projects: {}}
 end
+
+
+create_project_hash
+
+binding.pry
